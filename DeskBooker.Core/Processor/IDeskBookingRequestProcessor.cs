5a258f262cf89b3d385c8b8916ab87d0403d045d
hacker1db <hacker1db@outlook.com>
2020-12-08 21:53:15 -0800

    Finished Post method tests, and added Moq data so that our tests pass. We nwo can save data to the page using the OnPost Method.

---
+++ DeskBooker.Core/Processor/IDeskBookingRequestProcessor.cs
@@ -0,0 +1,9 @@
+using DeskBooker.Core.Domain;
+
+namespace DeskBooker.Core.Processor
+{
+    public interface IDeskBookingRequestProcessor
+    {
+        DeskBookingResult BookDesk(DeskBookingRequest request);
+    }
+}
--- DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
+++ DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
@@ -1,10 +1,41 @@
-﻿using NUnit.Framework;
+﻿using System;
+using DeskBooker.Core.Domain;
+using DeskBooker.Core.Processor;
+using Microsoft.CodeAnalysis.Operations;
+using Moq;
+using NUnit.Framework;
 
 namespace DeskBooker.Web.Pages
 {
   [TestFixture]
+  
   public class BookDeskModelTests
   {
+    [Test]
+    [TestCase(1, true)]
+    [TestCase(0,false)]
+    public void ShouldCallBookDeskMethodOfProcessorIfModelIsValid(
+      int expectedBookDesks, bool isModelValid)
+    {
+      // Arrange
+      var processorMock = new Mock<IDeskBookingRequestProcessor>();
+      var bookDeskModel = new BookDeskModel(processorMock.Object)
+      {
+        DeskBookingRequest = new DeskBookingRequest()
+      };
+
+      if (!isModelValid)
+      {
+        bookDeskModel.ModelState.AddModelError("JustAkey","AnErrorMessage");
+      }
+
+       // Act 
+        
+      bookDeskModel.OnPost();
+      //Assert
+      processorMock.Verify(x => x.BookDesk(bookDeskModel.DeskBookingRequest), 
+        Times.Exactly(expectedBookDesks));
+    }
 
   }
 }
